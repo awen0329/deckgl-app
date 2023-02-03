1058aace51f1162eb5546190dce1a9d53699d47c
Andrus <xwen0110@gmail.com>
2023-02-03 07:18:22 -0800

    feat: Implement control and statick panel

---
+++ src/components/CustomSlider.tsx
@@ -0,0 +1,66 @@
+import React from "react"
+import { styled } from "@mui/material/styles"
+import { Box, Grid, Typography, Slider, Input as MuiInput } from "@mui/material"
+
+const Input = styled(MuiInput)`
+  width: 42px;
+`
+
+interface CustomSliderProps {
+  label: string
+  value: number | string | Array<number | string>
+  // eslint-disable-next-line no-unused-vars
+  onChange: (value: number | string | Array<number | string>) => void
+}
+
+const CustomSlider: React.FC<CustomSliderProps> = ({ label, value, onChange }) => {
+  const handleSliderChange = (event: Event, newValue: number | number[]) => {
+    onChange(newValue)
+  }
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    onChange(event.target.value === "" ? 0 : Number(event.target.value))
+  }
+
+  const handleBlur = () => {
+    if (value < 0) {
+      onChange(0)
+    } else if (value > 100) {
+      onChange(100)
+    }
+  }
+
+  return (
+    <Box sx={{ maxWidth: 250, py: 4 }}>
+      <Typography id="input-slider" gutterBottom>
+        {label}
+      </Typography>
+      <Grid container spacing={2} alignItems="center">
+        <Grid item xs>
+          <Slider
+            value={typeof value === "number" ? value : 0}
+            onChange={handleSliderChange}
+            aria-labelledby="input-slider"
+          />
+        </Grid>
+        <Grid item>
+          <Input
+            value={value}
+            size="small"
+            onChange={handleInputChange}
+            onBlur={handleBlur}
+            inputProps={{
+              step: 10,
+              min: 0,
+              max: 100,
+              type: "number",
+              "aria-labelledby": "input-slider",
+            }}
+          />
+        </Grid>
+      </Grid>
+    </Box>
+  )
+}
+
+export default CustomSlider
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,7 +1,62 @@
-import React from "react"
+import React, { useState } from "react"
+import { Box, Button, Typography } from "@mui/material"
+import CustomSlider from "../components/CustomSlider"
+
+type ControlOptions = {
+  coverage: number
+  floorCount: number
+  floorHeight: number
+}
+
+type ControlOptionType = keyof ControlOptions
 
 const Dashboard = () => {
-  return <>Dashboard</>
+  const [controlOptions, setControlOptions] = useState<ControlOptions>({
+    coverage: 100,
+    floorCount: 6,
+    floorHeight: 40,
+  })
+
+  const handleControlOptionsChange =
+    (key: ControlOptionType) => (value: number | string | Array<number | string>) => {
+      setControlOptions((options) => ({
+        ...options,
+        [key]: value,
+      }))
+    }
+
+  return (
+    <Box sx={{ display: "flex", justifyContent: "space-between", px: 5 }}>
+      <Box sx={{ display: "flex", flexDirection: "column", minWidth: 250, flexShrink: 0, py: 4 }}>
+        <Button variant="contained">Load GeoJSON</Button>
+        <CustomSlider
+          label="Plot coverage %"
+          value={controlOptions.coverage}
+          onChange={handleControlOptionsChange("coverage")}
+        />
+        <CustomSlider
+          label="Floor number"
+          value={controlOptions.floorCount}
+          onChange={handleControlOptionsChange("floorCount")}
+        />
+        <CustomSlider
+          label="Floor height"
+          value={controlOptions.floorHeight}
+          onChange={handleControlOptionsChange("floorHeight")}
+        />
+      </Box>
+      <Box sx={{ flexShrink: 0, minWidth: "250px", height: "100vh", py: 4 }}>
+        <Typography variant="h5" gutterBottom color="textPrimary">
+          Statistiques
+        </Typography>
+        <Typography variant="subtitle1">Land Area (m2)</Typography>
+        <Typography variant="subtitle1">Building Area (m2)</Typography>
+        <Typography variant="subtitle1">Building Floor Area (m2)</Typography>
+        <Typography variant="subtitle1">Volume (m3)</Typography>
+        <Typography variant="subtitle1">Building Height (m)</Typography>
+      </Box>
+    </Box>
+  )
 }
 
 export default Dashboard
