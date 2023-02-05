f13d14f58dd4e82d9532c406df5cbac3ef258c18
Andrus <xwen0110@gmail.com>
2023-02-05 05:35:38 -0800

    Try to fix build error for mapbox-gl

--- craco.config.js
+++ craco.config.js
@@ -1,7 +1,10 @@
 module.exports = {
-  babel: {
-    loaderOptions: {
-      ignore: ["./node_modules/mapbox-gl/dist/mapbox-gl.js"],
-    },
+  module: {
+    rules: [
+      {
+        test: /\bmapbox-gl-csp-worker.js\b/i,
+        use: { loader: "worker-loader" },
+      },
+    ],
   },
 }
