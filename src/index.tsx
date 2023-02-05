617d452cc5df299c5e3cdb0c8045e590aa441dfb
Andrus <xwen0110@gmail.com>
2023-02-05 06:31:13 -0800

    Try to fix build error for mapbox-gl once more

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,6 @@ import ReactDOM from "react-dom/client"
 
 import App from "./App"
 import reportWebVitals from "./reportWebVitals"
-// @ts-ignore
-import mapboxgl from "mapbox-gl/dist/mapbox-gl-csp"
-// @ts-ignore
-import MapboxWorker from "worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker"
-
-mapboxgl.workerClass = MapboxWorker
 import "./index.css"
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
